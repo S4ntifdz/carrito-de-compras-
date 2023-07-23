b6cfc7c125d6aac27c3767454dc3de5581177ae4
S4ntifdz <safernandez@frro.utn.edu.ar>
2023-07-23 02:59:13 -0300

    Se agrego carousel de productos. se añadio la palabra deslizante en el tope de la pagina.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const carousel = document.querySelector('.carousel');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let slideIndex = 0;
+
+// Agregar evento click para la flecha izquierda
+leftArrow.addEventListener('click', () => {
+  slideIndex = Math.max(slideIndex - 1, 0);
+  updateSlidePosition();
+});
+
+// Agregar evento click para la flecha derecha
+rightArrow.addEventListener('click', () => {
+  const slidesCount = carousel.children.length;
+  slideIndex = Math.min(slideIndex + 1, slidesCount - 1);
+  updateSlidePosition();
+});
+
+function updateSlidePosition() {
+  const slideWidth = carousel.clientWidth;
+  carousel.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}
