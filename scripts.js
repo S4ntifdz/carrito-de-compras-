5db63f889aac40085a043332f1fb8a83eac3406d
S4ntifdz <safernandez@frro.utn.edu.ar>
2023-07-31 21:59:19 -0300

    ejemplo

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ const rightArrow = document.querySelector('.right-arrow');
 
 let slideIndex = 0;
 
-// Agregar evento click para la flecha izquierda
+//  flecha izquierda
 leftArrow.addEventListener('click', () => {
   slideIndex = Math.max(slideIndex - 1, 0);
   updateSlidePosition();
 });
 
-// Agregar evento click para la flecha derecha
+//  flecha derecha
 rightArrow.addEventListener('click', () => {
   const slidesCount = carousel.children.length;
   slideIndex = Math.min(slideIndex + 1, slidesCount - 1);
